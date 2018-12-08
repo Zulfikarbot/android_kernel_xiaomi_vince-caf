cda7dd47cd658ab0963e37abb0afa8a8d44285c4
Zulfikarbot <taitowatashiwa1@gmail.com>
2018-12-08 17:17:53 +0700

    Revert "kernel: Boost all CPUs to the max when userspace launches an app. Credits: sultanxda"
    dits: sultanxda
    
    This reverts commit 2882c2a859c1d39e8279575bc8adca4bd4320499.

--- fs/exec.c
+++ fs/exec.c
@@ -1568,11 +1568,9 @@ static int do_execve_common(struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	if (capable(CAP_SYS_ADMIN)) {
-		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
-			atomic_set(&zygote32_pid, current->pid);
-		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
-			atomic_set(&zygote64_pid, current->pid);
+	if (is_su && capable(CAP_SYS_ADMIN)) {
+		current->flags |= PF_SU;
+		su_exec();
 	}
 
 	/* execve succeeded */
